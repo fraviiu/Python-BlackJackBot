40284228718cfbc6d67bdc2d616580f5b72b9b92
fraviiu <63171227+fraviiu@users.noreply.github.com>
2020-04-18 10:01:45 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "1082852427:AAFixxqx0Kx0yDo1kx13RZRwgaDKHbheh_Q"
 USE_WEBHOOK = False
 WEBHOOK_PORT = 9001
-WEBHOOK_URL = "https://domain.example.com/" + BOT_TOKEN
+WEBHOOK_URL = "https://elsportifybot.herokuapp.com/" + BOT_TOKEN
 CERTPATH = "/etc/certs/example.com/fullchain.cer"
